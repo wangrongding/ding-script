852b67ea43b3e020e8303d11c8c68d10ef5f22a9
wangrongding <wangrongding@qq.com>
2021-07-21 23:26:30 +0800

    feat: 完善github一键在线编辑脚本

--- githubEditOnline.js
+++ githubEditOnline.js
@@ -1,35 +1,42 @@
 // ==UserScript==
-// @name         githubEditOnline
+// @name         github一键在线编辑
 // @namespace    https://github.com/wangrongding/ding-script
 // @version      0.1
-// @description  githubEditOnline
+// @description  github一键在线编辑
 // @author       汪荣顶
-// @homeurl      https://github.com/wangrongding/ding-script
+// @homeurl      https://github.com/wangrongding/ding-script/githubEditOnline.js
 // @license      MIT
-// @icon         https://www.google.com/s2/favicons?domain=github.com
-// @match        https://github.com/*
-// @require      file://E:\NoahSpace\Noah-Item\ding-script\githubEditOnline.js
+// @match        https://*.github.com/*/*
+// @icon         http://ww1.sinaimg.cn/large/75314ac9ly1gsoywgjgl3j205k05kaab.jpg
+// @require      file://E:\Noah-Programs\ding-script\githubEditOnline.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
     "use strict";
+    var btn = document.createElement("BUTTON");
+    btn.innerText = "在线编辑";
+    btn.style.cssText = `
+            position: fixed;
+            bottom: 100px;
+            right: 100px;
+            background:#282C34;
+            width: 100px;
+            height: 50px;
+            color:white;
+            z-index:999;
+            font-weight:bold;
+            font-size:15px; 
+            line-height:50px;
+            border-radius:50px;
+            `;
     setTimeout(() => {
-        var btn = document.createElement("BUTTON");
-        btn.innerText = "在线编辑";
-        btn.style.cssText = `
-        position: fixed;
-        bottom: 100px;
-        right: 100px;
-        background:#282C34;
-        width: 100px;
-        height: 50px;
-        line-height:50px;
-        color:white;
-        z-index:999;
-
-        `;
-        document.body.appendChild(btn);
-    }, 500);
+        btn.onclick = toOnlineEditor;
+    }, 2000);
+    document.body.appendChild(btn);
+    function toOnlineEditor() {
+        console.log("toOnlineEditor");
+        window.open(`${"https://github1s.com" + window.location.pathname}`);
+    }
     // Your code here...
 })();
--- tampermonkey-template.js
+++ tampermonkey-template.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name         Userscript Name
+// @name         UserScriptName
 // @namespace    https://github.com/wangrongding/ding-script
 // @version      0.1
-// @description  try to take over the world!
+// @description  yourProjectDescription
 // @author       汪荣顶
-// @homeurl      https://github.com/wangrongding/ding-script
+// @homeurl      https://github.com/wangrongding/ding-script/filePath
 // @license      MIT
-// @match        https://github.com/*
-// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @match        https://*.github.com/*
+// @icon         http://ww1.sinaimg.cn/large/75314ac9ly1gsoywgjgl3j205k05kaab.jpg
 // @grant        none
-// @require      file://E:\NoahSpace\Noah-Item\ding-script\tampermonkey-template.js
+// @require      file://localPath
 // ==/UserScript==
 
 (function () {
-    "use strict";
-    // Your code here...
+    "use stric";
+    //script template by wang'rong
+    //Your code here...
 })();
