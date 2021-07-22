a5ddd16d526c97564594dfec44efa450bcf1ae66
荣顶 <wangrongding@qq.com>
2021-07-22 11:34:48 +0800

    feat: 新增无限循环页面脚本,修改脚本模板

--- tampermonkey-template.js
+++ tampermonkey-template.js
@@ -5,15 +5,17 @@
 // @description  yourProjectDescription
 // @author       汪荣顶
 // @homeurl      https://github.com/wangrongding/ding-script/filePath
-// @license      MIT
+// @updateURL    https://github.com/wangrongding/ding-script/filePath
 // @match        https://*.github.com/*
+// @exclude      none
 // @icon         http://ww1.sinaimg.cn/large/75314ac9ly1gsoywgjgl3j205k05kaab.jpg
 // @grant        none
 // @require      file://localPath
+// @license      MIT
 // ==/UserScript==
 
 (function () {
     "use stric";
-    //script template by wang'rong
+    //script template by 汪荣顶
     //Your code here...
 })();
