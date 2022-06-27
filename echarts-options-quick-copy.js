648cac1ee0d0222c03392fad5ef045ae59d5b8b9
荣顶 <wangrongding@qq.com>
2022-06-27 15:02:42 +0800

    修改模板,和echarts快速复制的脚本

--- echarts-options-quick-copy.js
+++ echarts-options-quick-copy.js
@@ -1,35 +1,28 @@
 // ==UserScript==
-// @name         echarts-options-quick-copy
+// @name         echarts属性快速复制
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.0.1
+// @description  用于快速echarts配置文档中的默认属性到代码中修改.
+// @license      MIT
 // @author       You
 // @match        https://echarts.apache.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=apache.org
 // @grant        none
-// @require      file://E:\Noah-Programs\ding-script\echarts-options-quick-copy.js
+// @run-at       document-end
 // ==/UserScript==
-
+ 
+/* eslint-disable */
 (function () {
-  'use strict';
   window.onload = function () {
     document.querySelectorAll('.doc-content-item-card h4').forEach((item) => {
       item.querySelectorAll('.path-parent')?.forEach((title) => {
         title.textContent = title.textContent.replace(/\./g, ':{');
         if (item.querySelector('.default-value')) {
-          item.querySelector('.default-value').textContent = (
-            item.querySelector('.default-value').textContent + '}'
-          ).replace(/\=/g, ':');
+          item.querySelector('.default-value').textContent = `${
+            item.querySelector('.default-value').textContent
+          }}`.replace(/\=/g, ':');
         }
       });
-
-      const el = document.createElement('div');
-      el.innerText =
-        item.querySelector('.path-base').innerText +
-          ':' +
-          item.querySelector('.default-value')?.innerText || '-' + '}';
-      el.style.cssText = 'height:100px; 1px solid pink';
-      item.appendChild(el);
     });
   };
 })();
--- tampermonkey-template.js
+++ tampermonkey-template.js
@@ -1,21 +1,20 @@
 // ==UserScript==
-// @name         UserScriptName
+// @name         脚本名称
 // @namespace    https://github.com/wangrongding/ding-script
 // @version      0.1
-// @description  yourProjectDescription
-// @author       汪荣顶
-// @homeurl      https://github.com/wangrongding/ding-script/filePath
-// @updateURL    https://github.com/wangrongding/ding-script/filePath
-// @match        https://*.github.com/*
-// @exclude      none
-// @icon         http://ww1.sinaimg.cn/large/75314ac9ly1gsoywgjgl3j205k05kaab.jpg
-// @grant        none
-// @require      file://localPath
+// @description  脚本描述
+// @author       荣顶
 // @license      MIT
+// @exclude      none
+// @match        https://匹配的地址
+// @icon         https://assets.fedtop.com/picbed/202206271500878.png
+// @grant        none
+// @run-at       document-end
+// @require      file://本地文件路径
 // ==/UserScript==
 
 (function () {
   'use stric';
-  // script template by 汪荣顶
+  // script template by 荣顶
   // Your code here...
-}());
+})();
